aaf60bf0f0e9a42fb9869e7135bca60a6169e84e
Gryniv Ihor <37960904+gryniv@users.noreply.github.com>
2018-10-11 00:32:01 +0300

    Fix bugs
    - Add install all components
    - Fix download from git
    - Other mistake

--- hybris.sh
+++ hybris.sh
@@ -2,9 +2,12 @@
 gURL=$
 gitURL=
 hproject=doctors
-#install git mysql unzip
-sudo apt-get install git mysql-server mysql-client unzip -y
+#install services
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install git mysql-server mysql-client curl ant oracle-java8-installer unzip -y
 #download hybris gdrive
+mkdir -p ~/IdeaProjects/
 ggID=$(echo "$gURL" | egrep -o '(\w|-){26,}')
 ggURL='https://drive.google.com/uc?export=download'
 curl  -sc /tmp/gcokie "${ggURL}&id=${ggID}" >/dev/null
@@ -12,8 +15,10 @@ getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
 cmd='curl -o ~/IdeaProjects/"$hproject".zip --insecure -C - -LOJb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}"'
 echo -e "Downloading hybris from Google Drive..."
 eval $cmd
-#unzip hybris & download rep from git
-unzip ~/IdeaProjects/"$hproject".zip "hybris/*" -d ~/IdeaProjects/"$hproject" && git clone $gitURL
+#unzip hybris 
+unzip ~/IdeaProjects/"$hproject".zip "hybris/*" -d ~/IdeaProjects/"$hproject" 
+#download rep from git
+cd ~/IdeaProjects/"$hproject"/hybris;git init;git remote add origin $gitURL;git fetch;git checkout -t origin/master
 # create config dir & copy config-local to config
 mkdir -p ~/IdeaProjects/"$hproject"/hybris/config && cp -avr ~/IdeaProjects/"$hproject"/hybris/config-local/* ~/IdeaProjects/"$hproject"/hybris/config
 #take from config.prop data for DB
@@ -23,5 +28,5 @@ function prop {
 #mysql create DB from config file
 #download mysql driver from https://mvnrepository.com/artifact/mysql/mysql-connector-java/5.1.47
 wget -P ~/IdeaProjects/"$hproject"/hybris/bin/platform/lib/dbdriver/ "http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.47/mysql-connector-java-5.1.47.jar"
-#hybris generate && start server
-cd ~/IdeaProjects/"$hproject"/hybris/bin/platform;. ./setantenv.sh;ant clean all;ant initialize && . ./hybrisserver.sh
+#hybris generate
+cd ~/IdeaProjects/"$hproject"/hybris/bin/platform;. ./setantenv.sh;ant clean all;ant initialize
