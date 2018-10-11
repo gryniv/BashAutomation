235a5ddd8d590d6e398e6b4d7e7ab65f7122d580
Gryniv Ihor <37960904+gryniv@users.noreply.github.com>
2018-10-11 13:27:36 +0300

    add mysql auto create base & user

--- hybris.sh
+++ hybris.sh
@@ -2,10 +2,12 @@
 gURL=$
 gitURL=
 hproject=doctors
+urmysql=root
+prmysql=root
 #install services
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install git mysql-server mysql-client curl ant oracle-java8-installer unzip -y
+sudo apt-get install git mysql-server mysql-client curl oracle-java8-installer unzip -y
 #download hybris gdrive
 mkdir -p ~/IdeaProjects/
 ggID=$(echo "$gURL" | egrep -o '(\w|-){26,}')
@@ -26,6 +28,12 @@ function prop {
     grep "^\\s*${1}" ~/IdeaProjects/"$hproject"/hybris/config/local.properties|cut -d'=' -f2
 }
 #mysql create DB from config file
+mysql -u$urmysql -p$prmysql <<MYSQL_SCRIPT
+CREATE DATABASE $(prop 'db.name');
+CREATE USER $(prop 'db.username')@'localhost' IDENTIFIED BY $(prop 'db.password');
+GRANT ALL PRIVILEGES ON $(prop 'db.name').* TO $(prop 'db.username')@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
 #download mysql driver from https://mvnrepository.com/artifact/mysql/mysql-connector-java/5.1.47
 wget -P ~/IdeaProjects/"$hproject"/hybris/bin/platform/lib/dbdriver/ "http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.47/mysql-connector-java-5.1.47.jar"
 #hybris generate
