eb444a2f47597d9e20b676e9c7c6764444513cbe
Gryniv Ihor <37960904+gryniv@users.noreply.github.com>
2018-11-09 12:56:19 +0200

    Change mistakes
    - rename variables for more readable; 
    - delete unnecessary "hybris" folder;

--- hybris.sh
+++ hybris.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-gURL=$
-gitURL=
-gitbranch=
-hproject=doctors
+googleDriveURL=$
+gitCloneURL=
+gitbranch=master
+hybrisProjectName=doctors
 urmysql=root
 prmysql=root
 #install services
@@ -10,22 +10,22 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install git mysql-server mysql-client curl oracle-java8-installer unzip -y
 #download rep from git
-mkdir -p ~/IdeaProjects/"$hproject"/hybris/config && cd ~/IdeaProjects/"$hproject"/hybris;git init;git remote add origin $gitURL;git fetch;git checkout -t origin/$gitbranch
+mkdir -p ~/IdeaProjects/"$hybrisProjectName"/hybris/config && cd ~/IdeaProjects/"$hybrisProjectName";git init;git remote add origin $gitCloneURL;git fetch;git checkout -t origin/$gitbranch
 #download hybris gdrive
-ggID=$(echo "$gURL" | egrep -o '(\w|-){26,}')
+ggID=$(echo "$googleDriveURL" | egrep -o '(\w|-){26,}')
 ggURL='https://drive.google.com/uc?export=download'
 curl  -sc /tmp/gcokie "${ggURL}&id=${ggID}" >/dev/null
 getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
-cmd='curl -o ~/IdeaProjects/"$hproject".zip --insecure -C - -LOJb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}"'
+cmd='curl -o ~/IdeaProjects/"$hybrisProjectName".zip --insecure -C - -LOJb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}"'
 echo -e "Downloading hybris from Google Drive..."
 eval $cmd
 #unzip hybris 
-unzip ~/IdeaProjects/"$hproject".zip "hybris/*" -d ~/IdeaProjects/"$hproject" 
+unzip ~/IdeaProjects/"$hybrisProjectName".zip "hybris/*" -d ~/IdeaProjects/"$hybrisProjectName" 
 # create config dir & copy config-local to config
-cp -avr ~/IdeaProjects/"$hproject"/hybris/config-local/* ~/IdeaProjects/"$hproject"/hybris/config
+cp -avr ~/IdeaProjects/"$hybrisProjectName"/hybris/config-local/* ~/IdeaProjects/"$hybrisProjectName"/hybris/config
 #take from config.prop data for DB
 function prop { 
-    grep "^\\s*${1}" ~/IdeaProjects/"$hproject"/hybris/config/local.properties|cut -d'=' -f2
+    grep "^\\s*${1}" ~/IdeaProjects/"$hybrisProjectName"/hybris/config/local.properties|cut -d'=' -f2
 }
 #mysql create DB from config file
 mysql -u$urmysql -p$prmysql <<MYSQL_SCRIPT
@@ -35,6 +35,6 @@ GRANT ALL PRIVILEGES ON $(prop 'db.name').* TO $(prop 'db.username')@'localhost'
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 #download mysql driver from https://mvnrepository.com/artifact/mysql/mysql-connector-java/5.1.47
-wget -P ~/IdeaProjects/"$hproject"/hybris/bin/platform/lib/dbdriver/ "http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.47/mysql-connector-java-5.1.47.jar"
+wget -P ~/IdeaProjects/"$hybrisProjectName"/hybris/bin/platform/lib/dbdriver/ "http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.47/mysql-connector-java-5.1.47.jar"
 #hybris generate
-cd ~/IdeaProjects/"$hproject"/hybris/bin/platform;. ./setantenv.sh;ant clean all;ant initialize
+cd ~/IdeaProjects/"$hybrisProjectName"/hybris/bin/platform;. ./setantenv.sh;ant clean all;ant initialize
